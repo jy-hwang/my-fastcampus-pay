3b5457e068f90d1cd02e7e6f752109c09b24ab47
jyhwang <stratojy@gmail.com>
2025-01-20 23:40:14 +0900

    #3 - feat : Membership.Register
    1. 헥사고날 아키텍처
    2. 멤버십 등록을 위한 컨트롤러 - 서비스 - 리포지토리
    3. 간단한 테스트 코드

--- membership-service/src/main/java/com/fastcampuspay/membership/adapter/in/web/RegistMembershipController.java
+++ membership-service/src/main/java/com/fastcampuspay/membership/adapter/in/web/RegistMembershipController.java
@@ -1,15 +1,35 @@
 package com.fastcampuspay.membership.adapter.in.web;
 
+import com.fastcampuspay.membership.application.port.in.RegisterMembershipCommand;
+import com.fastcampuspay.membership.application.port.in.RegisterMembershipUseCase;
+import com.fastcampuspay.membership.domain.Membership;
+import common.WebAdapter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@WebAdapter
 @RestController
 @RequiredArgsConstructor
 public class RegistMembershipController {
 
-  @GetMapping("hello")
-  public String hello() {
-    return "hello";
+  private final RegisterMembershipUseCase registerMembershipUseCase;
+
+  @PostMapping("/membership/register")
+  Membership registerMembership(@RequestBody RegisterMembershipRequest request) {
+
+    RegisterMembershipCommand command
+            = RegisterMembershipCommand.builder()
+            .name(request.getName())
+            .address(request.getAddress())
+            .email(request.getEmail())
+            .isValid(true)
+            .isCorp(request.isCorp())
+            .build();
+
+
+    return registerMembershipUseCase.registerMembership(command);
+
   }
 }
